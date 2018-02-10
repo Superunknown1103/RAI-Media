b9e4d26e51e8421cc9ebecf7b78b1a49eed17acc
Superunknown1103 <superunknown1103@gmail.com>
2018-02-10 16:06:34 -0600

    rough draft, done for 2/10/2018

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,13 +2,25 @@ $(document).ready(function(){
 $('.subTitle').hide().fadeIn(3000);
 $('.subContent').hide().fadeIn(6000);
 
+var texts = ["Digital Video Production", "Search Engine Optimization", "Social Media Management"];
+count = 0;
+timer = setInterval(function() {
+        $(".subTitle").text(texts[count]);
+        count++;
+        if(count === texts.length) {
+            count=0;
+        }
+    }, 3000);
+imageChanger = setInterval(function() {
+     if ($('.subTitle').text() == "Social Media Management"){
+        $('.iframe').attr("src", "img/socialmedia.png");
+    }
+    else if ($('.subTitle').text() == "Search Engine Optimization"){
+       $('.iframe').attr("src", "img/optimization.png");
+    } 
+    else if ($('.subTitle').text() == "Digital Video Production"){
+        $('.iframe').attr("src", "img/tvAd.jpg");
+    }
+}, 3000);
+
 });
-var texts = ["test", "test2"];
-var count = 0;
-function changeText() {
-    count < 2 ? count++ : count = 0;
-    $(".subTitle").text(texts[count]);
-}
-
-
-setInterval(changeText(), 500);
