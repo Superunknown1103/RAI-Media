451ce910988ca7bfa033d79219d9101455cd7e05
Superunknown1103 <superunknown1103@gmail.com>
2018-02-10 14:49:37 -0600

    before express

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,14 @@
-$('.subTitle').hide().fadeIn(4000);
+$(document).ready(function(){
+$('.subTitle').hide().fadeIn(3000);
+$('.subContent').hide().fadeIn(6000);
+
+});
+var texts = ["test", "test2"];
+var count = 0;
+function changeText() {
+    $(".subTitle").text(texts[count]);
+    count < 2 ? count++ : count = 0;
+}
+
+
+setInterval(changeText(), 500);
